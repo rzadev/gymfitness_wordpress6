0a75c68ca8c19f0ce86c1c4a6d402ec723bd9c39
Mohammad Reza <rza9001@gmail.com>
2022-08-19 01:36:26 +0700

    Added blog homepage and single blog page

--- single.php
+++ single.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
-    <?php while(have_posts()): the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>            
-    <?php endwhile; ?>
+    <main class="container page section with-sidebar">
+        <div class="page-content">
+            <?php get_template_part('template-parts/page', 'loop'); ?>
+        </div>
+        <?php get_sidebar(); ?>
+    </main>
 
 <?php get_footer(); ?>
