569964fef4a1360c38651841c09762bf754c9348
Mohammad Reza <rza9001@gmail.com>
2022-08-28 14:04:19 +0700

    Added fixed menu

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,32 @@ jQuery(document).ready(function($){
         mode: 'fade'
     });
 
+        // When the page is ready add the fixed menu if position is greater than 300px
+        const headerScroll = document.querySelector('.navigation-bar');
+        const rect = headerScroll.getBoundingClientRect();
+        const topDistance = Math.abs(rect.top);
+        // console.log(topDistance);
+        fixedMenu(topDistance);
+
 });
+
+
+window.onscroll = () => {
+    let scroll = window.scrollY;
+    // console.log(scroll);
+    fixedMenu(scroll);
+
+}
+
+// Adds a fixed menu on top
+function fixedMenu(scroll) {
+    const headerScroll = document.querySelector('.navigation-bar');
+    // const documentBody = document.querySelector('body');
+
+    // In the case that the scroll is greater than 300 add some classes
+    if (scroll > 300) {
+        headerScroll.classList.add('fixed-top');
+    } else {
+        headerScroll.classList.remove('fixed-top');
+    }
+}
